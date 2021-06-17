585b8f38779467799ba82ece62ed945a01c9413f
Ronaldsantillan <86032264+Ronaldsantillan@users.noreply.github.com>
2021-06-16 21:02:01 -0500

    Add files via upload

---
+++ condiciones.py
@@ -0,0 +1,16 @@
+class condicion:
+    def __init__(self,num1=5,num2=4):
+        self.numero1=num1
+        self.numero2=num2
+        numero=self,numero1+nummero2
+        self.numero3=numero
+        
+    def usoif(self):
+        print(self.numero3)
+        
+print("instancia de la clse")
+cond1= condicion(10,20)
+cond2= condicion()
+print(cond2.numero3)
+cond1.usoif()
+print("gracias por su visita")
