c69b97d028074a0b9c31c4aa9edf648aabc919e6
Ronaldsantillan <86032264+Ronaldsantillan@users.noreply.github.com>
2021-07-05 21:58:33 -0500

    Add files via upload

---
+++ calcularjornada.py
@@ -0,0 +1,27 @@
+#S4 - Clase Virtual (2021-06-25)
+
+class Tarea:
+    def __init__(self):
+        pass
+    def calcularjornada(self):
+         ht, he, het=0,0,0
+         ph, phe, pt,ph8=0,0,0,0
+         ht= int(input("ingrese horas trabajadas: "))
+         ph= float(input("ingrese valor hora: "))
+         if ht > 40:
+             he = ht - 40
+             if he > 8:
+                het = he - 8
+                ph8 = 8*ph*2
+                phe = het*ph*3
+             else:
+                phe = 0
+                ph8 = he*ph*2
+             pt = 40*ph + ph8 + phe 
+         else:
+             pt =ht*ph
+         print("sobretiempo<8:{} sobretiempo>8:{} jornada:{} ".format(ph8,phe,pt))  
+               
+tarea = Tarea()
+tarea.calcularjornada()           
+                         
