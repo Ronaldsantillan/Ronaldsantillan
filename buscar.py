445290c304707b0cfaaf414dae9d700c5e2a1aaa
Ronaldsantillan <86032264+Ronaldsantillan@users.noreply.github.com>
2021-07-13 20:04:41 -0500

    Add files via upload

---
+++ buscar.py
@@ -0,0 +1,44 @@
+# S5 - Clase Virtual (2021-06-28)
+
+class ordenar:
+    def __init__ (self,lista):
+        self.lista=lista
+        
+    def recorrerElemento(self):
+        for ele in self.lista:
+            print(ele)
+            
+    def recorrerEnumerate(self):
+        for pos,ele in enumerate(self.lista):
+            print (pos,ele)
+    
+    def recorrerRange(self):
+        for pos in range(len(self.lista)):
+            print (pos,self.lista[pos])
+            
+    def buscar(self,buscado):
+        enc=False
+        for pos,ele in enumerate(self.lista):
+            if ele == buscado:
+                enc=True
+                break
+        if enc == True:
+            return pos
+        else:
+            return -1
+            
+    
+    
+lista = [2,3,1,5,8,10]
+ord1 = ordenar(lista)
+#ord1.recorrerElemento()
+#ord1.recorrerEnumerate()
+#ord1.recorrerRange()
+#print(ord1.buscar(9))
+buscado=9
+resp = ord1.buscar(buscado)
+if resp !=-1:
+    print("Numero={} se encuentra en la posicion:({})".format(buscado,resp))
+else:
+    print("Numero={} no se encuentra en la lista:({})".format(buscado,ord1.lista))
+
